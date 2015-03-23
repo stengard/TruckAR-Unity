26a51b6d5d57ec576756bfdad6268681882d4af3
Martin Stengård <mstengard@gmail.com>
2015-03-23 08:22:59 +0100

    hej

---
+++ Assets/Scenes/Loading And Unloading/FlashMaterial.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MeshRenderer))]
+public class FlashMaterial : MonoBehaviour {
+
+    public int flashingSpeed;
+
+    private float currentOpacity;
+
+    MeshRenderer meshRenderer;
+    // Use this for initialization
+    void Start() {
+        flashingSpeed = 10;
+        currentOpacity = 0;
+
+        meshRenderer = GetComponent<MeshRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        currentOpacity = Mathf.Abs(Mathf.Sin(Time.time*flashingSpeed));
+
+        meshRenderer.material.SetColor("_Color", new Color(meshRenderer.material.color.r, meshRenderer.material.color.g, meshRenderer.material.color.b, currentOpacity));
+        //DebugLog.Debugga(meshRenderer.material.color);
+    }
+}
